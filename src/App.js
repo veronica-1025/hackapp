71f463375c034ce3e4b03016066d503caba93238
Veronica <74000043+veronica-1025@users.noreply.github.com>
2022-05-14 16:17:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
+
 Amplify.configure(awsconfig)
 
 function App() {

df7b0b919d3d004caaf53ed1c899cb52d87124fc
Veronica <74000043+veronica-1025@users.noreply.github.com>
2022-05-14 16:17:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig)
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+      <AmplifySignOut />
+      <h2>My App Content</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
